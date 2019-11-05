07ea0993057b32fd3c0402b2e5dbe391a39d1edd
Taylor <taylor.winter@vuw.ac.nz>
2019-11-05 20:59:18 +1300

    Added comments on running models.

--- nzModel.R
+++ nzModel.R
@@ -1,15 +1,24 @@
-nzhs <- readRDS("nzhs.RDS") %>%
-  mutate(sexident = droplevels(replace(sexident, sexident %in% c("Don't know", "Refused answer", "Other"), NA)))
+library(tidyverse)
+library(brms)
 
-priors <- c(#prior("normal(-1.68, 0.5)", class="Intercept"),
-            prior("normal(-1.97, 0.5)", coef="age"),
-            prior("normal(-0.29, 0.1)", coef="sexM"),
-            prior("normal(-0.91, 0.4)", coef="nzdep2013"),
-            prior("normal(1.03, 0.18)", coef="sexidentGayDLesbian"),
-            prior("normal(1.18, 0.18)", coef="sexidentBisexual"),
-            prior("normal(-0.07, 0.2)", coef="sexM:sexidentGayDLesbian"),
-            prior("normal(-0.01, 0.2)", coef="sexM:sexidentBisexual"))
+# Load the data
+syntheticNz <- readRDS("data/syntheticNz.RDS")
 
+# Specify priors - I name the coefficient then specify a normal distribution to encompass where I think the true coefficient lies.
+# These are the coefficients reported in the US model and the errors have been doubled.
+priors <- c(
+  prior("normal(-1.97, 0.5)", coef="age"),
+  prior("normal(-0.29, 0.1)", coef="sexM"),
+  prior("normal(-0.91, 0.4)", coef="nzdep2013"),
+  prior("normal(1.03, 0.18)", coef="sexidentGayDLesbian"),
+  prior("normal(1.18, 0.18)", coef="sexidentBisexual"),
+  prior("normal(-0.07, 0.2)", coef="sexM:sexidentGayDLesbian"),
+  prior("normal(-0.01, 0.2)", coef="sexM:sexidentBisexual")
+)
+
+########################### Basic model with stress as outcome
+
+# Model stress as binomial outcome using US priors
 brms.test <- brm(k10high ~ age + sex * sexident + nzdep2013,
                        family = bernoulli(),
                        prior = priors,
@@ -18,6 +27,8 @@ brms.test <- brm(k10high ~ age + sex * sexident + nzdep2013,
                        chains = 4,
                        data=nzhs)
 
+# Model stress as binomial outcome without any priors (the large normal distribution just speeds up sampling).
+# This looks analogous to a frequentist logistic regression.
 brms.test.naive <- brm(k10high ~ age + sex * sexident + nzdep2013,
                  family = bernoulli(),
                  prior = prior("normal(0, 5)", class = "b"),
@@ -26,15 +37,13 @@ brms.test.naive <- brm(k10high ~ age + sex * sexident + nzdep2013,
                  chains = 4,
                  data=nzhs)
 
-priors <- c(prior("normal(-1.68, 0.5)", class="Intercept", resp="k10high"),
-            prior("normal(-1.97, 0.5)", coef="age", resp="k10high"),
-            prior("normal(-0.29, 0.1)", coef="sexM", resp="k10high"),
-            prior("normal(-0.91, 0.4)", coef="nzdep2013", resp="k10high"),
-            prior("normal(1.03, 0.18)", coef="sexidentGayDLesbian", resp="k10high"),
-            prior("normal(1.18, 0.18)", coef="sexidentBisexual", resp="k10high"),
-            prior("normal(0.7, 0.35)", coef="sexidentGayDLesbian", resp="hazardous"),
-            prior("normal(0.7, 0.35)", coef="sexidentBisexual", resp="hazardous"))
+# Save out models
+saveRDS(brms.test, "nzAnalysis20190428.RDS")
+saveRDS(brms.test.naive, "nzAnalysisNaive20190428.RDS")
 
+########################### Mediation model with stress as outcome
+
+# Mediation model with same US priors
 brms.test.mediation <- brm(bf(hazardous ~ age + sex + sexident + nzdep2013 + k10high) +
                              bf(k10high ~ age + sex + sexident + nzdep2013),
                            family = bernoulli(),
@@ -44,6 +53,7 @@ brms.test.mediation <- brm(bf(hazardous ~ age + sex + sexident + nzdep2013 + k10
                            chains = 4,
                            data=nzhs)
 
+# Mediation model with no priors
 brms.test.mediation.naive <- brm(bf(hazardous ~ age + sex + sexident + nzdep2013 + k10high) +
                              bf(k10high ~ age + sex + sexident + nzdep2013),
                            family = bernoulli(),
@@ -51,8 +61,6 @@ brms.test.mediation.naive <- brm(bf(hazardous ~ age + sex + sexident + nzdep2013
                            chains = 4,
                            data=nzhs)
 
-saveRDS(brms.test, "nzAnalysis20190428.RDS")
-saveRDS(brms.test.naive, "nzAnalysisNaive20190428.RDS")
-
+# Save out models
 saveRDS(brms.test.mediation, "nzMediation20190428.RDS")
 saveRDS(brms.test.mediation.naive, "nzMediationNaive20190428.RDS")
--- usModel.R
+++ usModel.R
@@ -1,17 +1,21 @@
 library(tidyverse)
 library(brms)
 
-syntheticUs <- readRDS("data/syntheticNz.RDS")
+# Load the data
+syntheticUs <- readRDS("data/syntheticUs.RDS")
 
+# Make some weakly informative priors purely to speed up sampling
 priors <- c(prior("normal(1, 1)", coef="IRSEX2"),
             prior("normal(-1, 1)", coef="moAGE2"),
             prior("normal(-1, 1)", coef="moIRFAMIN3"))
 
+# Run basic brms model with stress as a binomial outcome. mo() denotes a monotonic effect.
 brms.test <- brm(SPDMON ~ mo(AGE2) + IRSEX * SEXIDENT + mo(IRFAMIN3),
                  family = bernoulli(),
                  prior = priors,
                  cores = 4,
                  chains = 4,
-                 data=dat)
+                 data=syntheticUs)
 
+# Save out the model
 saveRDS(brms.test, "usAnalysis20190320.RDS")
